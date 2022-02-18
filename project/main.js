43024c4be922a44455e163f5ca8b1c6ab143cee9
Austin Hwang <austverse@gmail.com>
2022-02-19 01:49:31 +0900

    first commit

---
+++ project/main.js
@@ -0,0 +1,14 @@
+// let nftRarity = document.querySelector('.rarity')
+
+// window.addEventListener('scroll', function (){
+//   let value = window.scrollY
+//   console.log('scrollY', value);
+
+//   if ((value > 3731) || (value < 2900)) {
+//     nftRarity.style.animation='nft-slideout 1s forwards'
+//   } else {
+//     nftRarity.style.animation='nft-slidein 1s'
+//   }
+
+// })
+
